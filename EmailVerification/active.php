0b26e866f813a317309c67fa1ccc56499ae66903
Rizwan Ahmed <46168791+Rizwan8909@users.noreply.github.com>
2020-08-18 11:54:16 -0700

    Email verification is done

--- EmailVerification/active.php
+++ EmailVerification/active.php
@@ -1,1 +1,23 @@
-Hello from active
+<?php
+    include "db_connection.php";
+    session_start();
+    if(isset($_GET['token'])){
+        $token = $_GET['token'];
+
+        $update = "UPDATE `users` SET `status` = 'active' WHERE `token` = '$token'";
+        $run_update = mysqli_query($conn, $update);
+
+        if($run_update){
+            $_SESSION['msg'] = "Your account is now activated. You can now log in";
+            header("location: login.php");
+        }
+        else{
+            $_SESSION['msg'] = "You are now logged out";
+            header("location: login.php");
+        }
+    }
+    else{
+        $_SESSION['msg'] = "Can't activate your account";
+        header("location: login.php");
+    }
+?>
